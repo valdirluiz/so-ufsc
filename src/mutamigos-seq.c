f90e547e2d0665602686fe1a30ee618ca057d1e8
Valdir <arnholdvaldir@gmail.com>
2016-04-29 19:57:16 -0300

    Update mutamigos-seq.c
    ajustes

--- src/mutamigos-seq.c
+++ src/mutamigos-seq.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- 
 
-void  somaDivisores(int numero, int teste, int *result)
+
+int  somaDivisores(int numero, int teste)
 {
+  int result = 0;
 	while(teste<=numero){
 		if(numero % teste == 0){
-			*result = *result+teste;
+			result = result+teste;
 		}
 		teste = teste+1;
 	}
-	 
+  return result;
+
 }
 
 int main(int argc, const char *argv[])
 {
-		int resultado;
-		int min = atoi(argv[1]);
-        int max = atoi(argv[2]);
-        int tamanhoBuffer = max-min+1;
 
 
-        //TODO: criar funcao para poular buffer com os numeros que precisa ser somados os divisores
-        int buffer[tamanhoBuffer][2];
-        int colunaNoBuffer = 0;
-        while(min<=max){
-        	buffer[0][colunaNoBuffer] = min;
-        	printf("%d\n", buffer[0][colunaNoBuffer]);
-        	min=min+1;
-        	colunaNoBuffer=colunaNoBuffer+1;
-        }
-
-        //TODO: percorrer buffer e calcular divisores, pode ser em paralelo
-
-        //TODO: verificar numeros no buffer que tem a linha 1 igual e imprimir
+    int min = atoi(argv[1]);
+    int max = atoi(argv[2]);
+    int tamanhoBuffer = max-min+1;
 
 
+    float buffer[2][tamanhoBuffer];
 
 
- 
+    int count = 0;
+
+    while (count<tamanhoBuffer) {
+      buffer[0][count] = min;
+
+      int dividendo = somaDivisores(min, 1);
+      float resultado = (float) dividendo/min;
+      buffer[1][count] = resultado;
+      //TODO: popular tabela
+      printf("%f\n", resultado);
+      count=count+1;
+      min=min+1;
+    }
+
+    //TODO: imprimi
+
+
+
+
+
 		return 0;
 }
